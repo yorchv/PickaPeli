9c3d01a544edd77493969dacd018dd1725128925
Jorge Vivas <yorch.vivas@gmail.com>
2012-06-26 13:04:23 -0500

    Commit message

---
+++ application/controllers/welcome.php
@@ -0,0 +1,20 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('url');
+		//$this->load->library('tank_auth');
+	}
+
+	function index()
+	{
+		$this->load->view('welcome');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
